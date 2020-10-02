33e76d0020697302b82f9af662f507cbc2d6c016
dhara8128 <72105112+dhara8128@users.noreply.github.com>
2020-10-02 15:15:48 +0530

    Add files via upload

---
+++ java/  Easy/UsingClasses.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    Person.hello();
+  }
+}
+
+
+
+
+
+
+
+
+class Person {
+  public static void hello() {
+    // Replace "Hello World" with "Hello Java"
+    System.out.println("Hello Java");
+  }
+}
+
